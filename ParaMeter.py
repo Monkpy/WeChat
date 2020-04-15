59b7222e41f4682a4e7d247defe02b6514a439b5
Monkpy <34485630+Monkpy@users.noreply.github.com>
2020-04-15 16:06:47 +0800

    拼接真URL
    解析js方法完成URL拼接

--- ParaMeter.py
+++ ParaMeter.py
@@ -15,18 +15,21 @@ this.href+="&k="+b+"&h="+a)})})();
 """
 import random
 
+import requests
+
 
 def paraMeter(url_str):
 
-    b = int(random.random()*100) + 1
+	b = int(random.random()*100) + 1
 
-    url = url_str
+	url = url_str
 
-    a = url.find('url=')
-    url_old = url + '&k=' + str(b) + '&h=' + url[a + 4 + 21 + b: a + 4 + 21 + b + 1]
-    url_new = 'https://weixin.sogou.com' + url_old
-    print(url_new)
-    return url_new
+	a = url.find('url=')
+	url_old = url + '&k=' + str(b) + '&h=' + url[a + 4 + 21 + b: a + 4 + 21 + b + 1]
+	url_new = 'https://weixin.sogou.com' + url_old
+	# print(url_new)
+	return url_new
+
 
 
 
