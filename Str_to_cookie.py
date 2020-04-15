c9fc55539274474cb97b7b813e9a669786b83390
Monkpy <34485630+Monkpy@users.noreply.github.com>
2020-04-15 16:07:34 +0800

    获取cookie的URL链接参数

---
+++ Str_to_cookie.py
@@ -0,0 +1,35 @@
+# - *- coding:utf-8 -*-
+
+str_url_dict = {
+	"uigs_refer": "https://weixin.sogou.com/",
+	"weixin?query": "旅游攻略",
+	"_sug_type_": "",
+	"s_from": "input",
+	"_sug_": "n",
+	"type": "2",
+	"ie": "utf8",
+	"uigs_t": "",  # 时间戳
+	"uigs_productid": "vs_web",
+	"terminal": "web",
+	"vstype": "weixin",
+	"pagetype": "result",
+	"channel": "result_article",
+	"s_from": "input",
+	"sourceid": "",
+	"type": "weixin_search_pc",
+	"uigs_cookie": "SUID,sct",
+	"uuid": "",  # 唯一性认证
+	"query": "旅游攻略",
+	"weixintype": "2",
+	"exp_status": "-1",
+	"exp_id_list": "0_0",
+	"wuid": "002502EE314DDDD05C061FC295282991",
+	"snuid": "",  # 列表set-cookie中获取
+	"rn": "1",
+	"login": "0",
+	"uphint": "1",
+	"bottomhint": "1",
+	"page": "1",
+	"right": "right0_0",
+	"exp_id": "null_0-null_1-null_2-null_3-null_4-null_5-null_6-null_7-null_8-null_9",
+}
